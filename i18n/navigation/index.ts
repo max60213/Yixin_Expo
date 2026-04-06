c3f451c464d9a06333e02f426c952fee5ed721e7
max60213 <max60213@gmail.com>
2026-04-06 13:43:48 +0800

    chore: remove unused next-intl/navigation module

--- i18n/navigation/index.ts
+++
@@ -1,9 +0,0 @@
-import {createNavigation} from 'next-intl/navigation';
-
-// Light wrapper, wrapping Next.js routing API
-export const {Link, redirect, usePathname, useRouter, getPathname} = createNavigation({
-  locales: ['en', 'zh-TW'],
-  defaultLocale: 'en',
-});
-
-export default Link;
